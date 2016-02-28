bb1421c1c5760e93c65d0f1195de0449969902df
Sathibabu-P <sathipatchipala@gmail.com>
2016-02-28 16:14:22 +0530

    add rating and likes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
   belongs_to :room
-  has_attached_file :attachment, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :attachment, styles: { medium: "800x800>", thumb: "180x180>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\Z/
 end
